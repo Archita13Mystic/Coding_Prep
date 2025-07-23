c31afb00af915468669a0cd34c1a448df57d18a0
Archita13Mystic <archita132004@gmail.com>
2025-07-24 00:28:53 +0530

    sorting

---
+++ YT/yt dsa/Sorting/insertion_sort.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    
+    vector<int> v(n,0);
+
+    for(int i=0;i<n;i++){
+        cin >> v[i];
+    }
+    for(int i=0;i<n;i++){
+        int j = i;
+        while(j > 0 && v[j] < v[j-1]){
+            swap(v[j],v[j-1]);
+            j--;
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        cout << v[i] << " ";
+    }
+}
